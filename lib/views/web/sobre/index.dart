2e7e0014154e698a6b1fdd8baa59b8184ef52b67
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2021-09-14 17:02:04 -0300

    sobre

--- lib/views/web/sobre/index.dart
+++ lib/views/web/sobre/index.dart
@@ -34,21 +34,72 @@ class _SobrePageState extends State<SobrePage> {
                 ),
               ),
             ),
-
-            Container(
-              child: Center(
-                child: Text(
-                  'Nome',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.black
-                  ),
-                ),
-              ),
-            )
+            _titulo('NOME'),
+            _texto('SINTEU - Sistema de Integração Tecnológica Empresa/Universidade'),
+            _titulo('PROJETO EM UMA FRASE'),
+            _texto('Conexão de empresários com as universidades de uma forma mais simples.'),
+            _titulo('PROBLEMA'),
+            _texto(
+              'Muitos alunos e professores têm dificuldade de se conectar com empresas em busca de'    
+              +'projetos científicos ou estágios. As empresas, por sua vez, também possuem dificuldades'
+              +'em encontrar profissionais que melhor atendam suas demandas, visto que elas nem sempre '
+              +'chegam a serem conhecidas. De acordo com uma pesquisa realizada pela equipe,'
+              +'concluímos que a cada 10 pessoas do meio acadêmico, 8 já tiveram problemas para se'
+              +'conectar com empresas, e muitas dessas empresas não possuem uma indicação válida'
+              +'para contratar seus funcionários. Além disso, de acordo com dados do Ministério da'
+              +'Educação, o Brasil tem cerca de 17 milhões de estudantes com possibilidade de estagiar,'
+              +'mas apenas 1 milhão deles conseguem garantir uma vaga. Isso acaba dificultando a '
+              +'inserção destas pessoas no mercado de trabalho.'
+            ),
+            _titulo('SOLUÇÃO'),
+            _texto(
+              'Uma plataforma online em que as empresas vão encontrar funcionários que possuem os'
+              +'requisitos desejados com mais facilidade, além de poderem usar as soluções propostas'
+              +'pelas instituições de ensino e que podem ser usadas em diversos segmentos de mercado,'
+              +'como na resolução de demandas das empresas. Através dela, os membros das'
+              +'universidades entrarão em contato mais facilmente com as empresas para trabalharem'
+              +'juntos em pesquisas, projetos de conclusão de curso, estágios e afins.'
+            ),
+            
+            SizedBox(height: 100),
           ],
         ),
+    );
+  }
+
+  Widget _titulo(texto){
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Center(
+          child: Text(
+            texto,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w900,
+              color: Colors.black
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _texto(texto){
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 150, right: 150, bottom: 15),
+        child: Center(
+          child: Text(
+            texto,
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
